8f3c3bae8d9ad620dfae1caa0dd328b92d571ccb
Thuong Ho <thuong.t.ho@gmail.com>
2016-03-21 17:26:57 -0700

    add jquery datatables

---
+++ popup.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('#itemCounts').click(function () {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: 'itemCounts'});
+    });
+  });
+
+  $('#makeSortable').click(function () {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: 'makeSortable'});
+    });
+  });
+});
--- ps_cs.js
+++ ps_cs.js
@@ -1,1 +1,31 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // goes through and finds all the authors and counts the titles
+  if (request.action == "itemCounts") {
+    var authors = $('.search-result__author a');
+    var hist = {};
+
+    authors.map(function (a) {
+      if (a.title in hist) {
+        hist[a.title]++;
+      } else {
+        hist[a.title] = 1;
+      }
+    });
+
+    for (var a in hist) {
+      var aName = '"' + a + '"';
+      var links = $('.search-result__author a[title=' + aName + ']');
+
+      $.each(links, function (i, v) {
+        v.innerText = a + ' - ' + hist[a];
+      });
+    }
+  }
+
+  if (request.action == "makeSortable") {
+    // $('.course').dataTable({ "bPaginate": false });
+    $('.search-result__title').dataTable({ "bPaginate": false });
+  }
+});
+
 chrome.runtime.sendMessage({action:"show"});
